77c0c894e935acf60efc0f4f12a49788802d4a11
karming-leong leong <karming.leong@gmail.com>
2025-03-24 10:35:45 +0000

    updating backend

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const productsRouter = require('./routes/products');
+const userRoutes = require('./routes/users');
+const cartRoutes = require('./routes/cart');
+
+const pool = require('./database');
+
+
+const app = express();
+
+
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+
+//routes
+app.use('/api/products', productsRouter);
+app.use('/api/users', userRoutes);
+app.use('/api/cart', cartRoutes);
+
+// Routes(basic)
+app.get('/', (req, res) => {
+  res.json({ message: "Welcome to the ecommerce API" });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/products.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../database');
+
+// GET all products
+router.get('/', async (req, res) => {
+    try {
+        const products = await productService.getAllProducts();
+        res.json(products);
+      } catch (error) {
+  res.status(500).json({ message: "error.message" });
+      }
+});
+
+// GET a single product
+router.get('/:id', async (req, res) => {
+    try{
+        const product = await productService.getProductById(req.params.id);
+        res.json(product);
+    }
+    catch{
+  res.status(500).json({ message: "error.message" });
+    }
+});
+
+module.exports = router;
